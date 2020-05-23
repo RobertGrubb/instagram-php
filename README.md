b050aaeed4041c49e8194d0b5f40b839a5bddcda
Matt Grubb <mgrubb0507@gmail.com>
2020-05-23 16:13:07 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instagram Scraper in PHP
+
+Work in progress, will be committing as much as I can as soon as I can. 
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'vendor/autoload.php';
+require_once __DIR__ . '/src/Instagram.php';
+
+use Instagram\Scraper;
+
+// Instantiate Instagram Scraper library
+$Scraper = new Scraper();
